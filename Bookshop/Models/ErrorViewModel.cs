b8df6bacd6441b579b305b98c5c1ffb26600fb71
Denis G <denis.g@engagecraft.com>
2023-04-09 23:37:19 +0300

    feat: create project

---
+++ Bookshop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Bookshop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
