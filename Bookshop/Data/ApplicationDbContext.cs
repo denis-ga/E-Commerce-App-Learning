191d6e9dcb358f51428ff41b6e5469e9bdf7cbcf
Denis G <denis.g@engagecraft.com>
2023-04-30 00:30:41 +0300

    feat: add entity framework

---
+++ Bookshop/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Bookshop.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bookshop.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
